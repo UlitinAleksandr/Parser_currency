53ca5de634bb81ca818ad5379aa883c0ac12f9cf
alek-ulitin <alek-ulitin@yandex.ru>
2022-03-13 21:24:37 +0300

    init

--- parser_currency.py
+++ parser_currency.py
@@ -3,14 +3,9 @@ from bs4 import BeautifulSoup
 import time
 
 
-DOLLAR_RUB = "https://www.google.com/search?q=%D0%BA%D1%83%D1%80%D1%81+%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80%D0%B0&oq=" \
-                 "%D0%BA%D1%83%D1%80%D1%81+&aqs=chrome.0.69i59l3j35i39j69i57j69i61l3.1027j1j9&sourceid=chrome&ie=UTF-8"
-EURO_RUB = "https://www.google.com/search?q=rehc+tdhj&oq=rehc+tdhj&aqs=chrome" \
-               "..69i57j0i10i131i395i433l4j0i10i131i433j0i10j0i10i433j0i10j0i10i131i433.1277j1j7&sourceid=chrome&i" \
-           "e=UTF-8"
-LIRA_RUB = "https://www.google.com/search?q=%D0%BA%D1%83%D1%80%D1%81+%D0%BB%D0%B8%D1%80%D1%8B+%D0%BA+%D1%" \
-               "80%D1%83%D0%B1%D0%BB%D1%8E&oq=%D0%BA%D1%83%D1%80%D1%81+%D0%BB%D0%B8%D1%" \
-               "80%D1%8B&aqs=chrome.0.69i59j69i57j0i512j0i20i263i512j0i512l6.3138j1j9&sourceid=chrome&ie=UTF-8"
+DOLLAR_RUB = "https://www.google.com/search?q=%D0%BA%D1%83%D1%80%D1%81+%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80%D0%B0"
+EURO_RUB = "https://www.google.com/search?q=rehc+tdhj&oq"
+LIRA_RUB = "https://www.google.com/search?q=%D0%BA%D1%83%D1%80%D1%81+%D0%BB%D0%B8%D1%80%D1%8B"
 Headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"" Chrome/"
                          "99.0.4844.51 Safari/537.36"}
 
